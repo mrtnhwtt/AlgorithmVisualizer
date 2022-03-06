687d791c5967a193a7c781a4289368bac3e82b04
Martin Hewitt <martin1.hewitt@epitech.eu>
2022-03-06 19:42:54 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [r_array, setRArray] = useState([]);
+  const createUnsortedArray = () => {
+    for (var array=[],i=0;i<100;++i) array[i]=i;
+    var tmp, current, top = array.length;
+    if(top) while(--top) {
+      current = Math.floor(Math.random() * (top + 1));
+      tmp = array[current];
+      array[current] = array[top];
+      array[top] = tmp;
+    }
+    console.log(array)
+    setRArray(array);
+  };
+  useEffect(() => {
+    console.log(r_array.length)
+    if(r_array.length === 0) {
+      createUnsortedArray()
+      // console.log(r_array)
+    }
+  }, [r_array]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="nav-title">
+          <div className="App-title">Pretty Array Visualisation</div>
+          <div className="button-container">
+            <button>Regenerate Array</button>
+          </div>
+        </div>
       </header>
+        <main>
+          Array 
+        </main>
     </div>
   );
 }
